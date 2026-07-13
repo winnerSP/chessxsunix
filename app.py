329634814dbd23b603d7da671dc4f1f97d044e57
winnerSP <sanay.paliwals@gmail.com>
2026-07-13 20:19:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+from database import connect
+
+
+@app.route("/add_player/<name>/<rating>")
+def add_player(name, rating):
+
+    db = connect()
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "INSERT INTO players(username, rating, games) VALUES (?, ?, ?)",
+        (name, int(rating), 0)
+    )
+
+    db.commit()
+    db.close()
+
+    return {
+        "message": "Player Added 🚀"
+    }
+
 
 @app.route("/")
 def home():
